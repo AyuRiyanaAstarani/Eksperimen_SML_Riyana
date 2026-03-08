280a2724ca15056e70d9817ac130f5f9be91f852
Ayu Riyana Astarani <astarani.2308561031@student.unud.ac.id>
2026-03-08 11:29:30 +0800

    improve mlflow logging for skilled criteria

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -2,13 +2,14 @@ import pandas as pd
 import mlflow
 import mlflow.sklearn
 
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
 mlflow.set_tracking_uri("sqlite:///mlflow.db")
-mlflow.set_experiment("Churn_Experiment")
+mlflow.set_experiment("Churn_Model")
+
+mlflow.sklearn.autolog()
 
 train = pd.read_csv("train_preprocessed.csv")
 test = pd.read_csv("test_preprocessed.csv")
@@ -19,9 +20,8 @@ y_train = train["Exited"]
 X_test = test.drop("Exited", axis=1)
 y_test = test["Exited"]
 
-mlflow.set_experiment("Churn_Model")
 
-with mlflow.start_run():
+with mlflow.start_run(run_name="RandomForest_Model"):
 
     model = RandomForestClassifier(
         n_estimators=100,
@@ -29,8 +29,10 @@ with mlflow.start_run():
         random_state=42
     )
 
+  
     model.fit(X_train, y_train)
 
+    
     y_pred = model.predict(X_test)
 
     acc = accuracy_score(y_test, y_pred)
@@ -38,18 +40,10 @@ with mlflow.start_run():
     recall = recall_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred)
 
-    
-    mlflow.log_param("n_estimators", 100)
-    mlflow.log_param("max_depth", 5)
+    mlflow.log_metric("test_accuracy", acc)
+    mlflow.log_metric("test_precision", precision)
+    mlflow.log_metric("test_recall", recall)
+    mlflow.log_metric("test_f1_score", f1)
 
-    
-    mlflow.log_metric("accuracy", acc)
-    mlflow.log_metric("precision", precision)
-    mlflow.log_metric("recall", recall)
-    mlflow.log_metric("f1_score", f1)
-
-    # log model
-    mlflow.sklearn.log_model(model, "random_forest_model")
-
-print("Training selesai")
-print("Accuracy:", acc)
+    print("Training selesai")
+    print("Accuracy:", acc)
